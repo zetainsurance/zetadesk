4aa9dde66b2c2471baae1e093f64b4afdd760b0e
zetainsurance <working.zetainsurance@gmail.com>
2026-07-27 15:15:01 +0530

    Delete app directory

--- app/page.js
+++
@@ -1,4 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const ZetaDesk = dynamic(() => import("@/components/ZetaDesk"), { ssr: false });
-export default function Page() { return <ZetaDesk />; }
